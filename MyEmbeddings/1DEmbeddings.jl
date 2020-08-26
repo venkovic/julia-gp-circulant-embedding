fd7f5cd0fb29271837c2bb029b4616a6c5edcb50
nvenkov1 <venkovic@gmail.com>
2020-08-27 01:12:24 +0200

    Mostly comments, and sample()

--- MyEmbeddings/1DEmbeddings.jl
+++ MyEmbeddings/1DEmbeddings.jl
@@ -1,11 +1,24 @@
 using FFTW: rfft, irfft
 using LinearAlgebra: dot
 
+"""
+bststb_1d(n, e1)
+
+Structure for symmetric n-by-n Toeplitz matrix. Only the first column e1 of the
+matrix is stored.
+"""
 struct bststb_1d
   n::Int
   e1::Vector{T}
 end
 
+"""
+matvec(Toe, x)
+
+Performs a (naive) matrix-vector product between a symmetric n-by-n Toeplitz
+matrix Toe and a vector x. The complexity remains O(n^2), even though only the
+first column of the matrix is stored.
+"""
 function matvec(Toe::bststb_1d, x::Vector{T})
   Toe_x = zeros(T, Toe.n)
   for i in 1:Toe.n
@@ -15,6 +28,12 @@ function matvec(Toe::bststb_1d, x::Vector{T})
   return Toe_x
 end
 
+"""
+get_cov_toeplitz_1d(n, cov)
+
+Assembles a Toeplitz structure of a stationnary covariance cov(Δx) evaluated
+at Δx = 0, 1 / (n - 1), ..., (n - 2) / (n - 1), 1.
+"""
 function get_cov_toeplitz_1d(n::Int, cov)
   dx = 1 / (n - 1)
   Te1 = Vector{T}(undef, n)
@@ -27,19 +46,29 @@ end
 import Base: *
 (*)(Toe::bststb_1d, x::Vector{T}) = matvec(Toe::bststb_1d, x::Vector{T})
 
+"""
+bscscb_1d(n, m, hCe1, n_pos)
 
+Structure for symmetric m-by-m circulant embedding of a symmetric n-by-n
+Toeplitz matrix. Only the DFT hCe1 of the first column of the circulant matrix
+is stored.
+"""
 struct bscscb_1d
   n::Int
-  #Ce1::Vector{T}
   m::Int
   hCe1::Vector{T}
-  n_pos::Int
 end
 
 function get_pos_evals(C::bscscb_1d)
   nothing
 end
 
+"""
+matvec(C, x)
+
+Performs a matrix-vector product between a n-by-n symmetric Toeplitz matrix Toe
+and a vector x, using a circulant embedding structure C of Toe.
+"""
 function matvec(C::bscscb_1d, x::Vector{T})
   x_0_padded = zeros(T, C.m)
   x_0_padded[1:C.n] = x
@@ -48,7 +77,75 @@ function matvec(C::bscscb_1d, x::Vector{T})
   return q[1:C.n]
 end
 
+"""
+sample(C)
+
+Draws a realization of the n-by-n symmetric Toeplitz matrix Toe, using a
+circulant embedding structure C of Toe.
+"""
+function sample(C::bscscb_1d)
+  x_0_padded = zeros(T, C.m)
+  x_0_padded[1:C.n] = randn(T, C.n)
+  y = sqrt.(C.hCe1) .* rfft(x_0_padded)
+  q = irfft(y, C.m)
+  return q[1:C.n]
+end
+
+"""
+get_mce_1d(Toe)
+
+Assembles the m-by-m minimally circulant embedding structure of a given symmetric
+n-by-n Toeplitz structure (m = 2 * (n - 1)).
+"""
 function get_mce_1d(Toe::bststb_1d)
+  m = 2 * (Toe.n - 1)
+  #
+  Ce1 = Vector{T}(undef, m)
+  Ce1[1:Toe.n] = Toe.e1
+  for j in 2:(Toe.n-1)
+    Ce1[2 * (Toe.n - 1) - j + 2] = Ce1[j]
+  end
+  #
+  hCe1 = real(rfft(Ce1))
+  #
+  trace_pos = 0.
+  n_pos = Toe.n
+  for j in 1:Toe.n
+    al = hCe1[j]
+    if al > 2 * eps(T)
+      if j > 1
+        trace_pos += 2 * al
+      else
+        trace_pos += al
+      end
+    else
+      n_pos = j - 1
+      break
+    end
+  end
+  hCe1[n_pos+1:end] .= 0
+  energy = trace_pos / (2 * (Toe.n - 1)), n_pos
+  #
+  return bscscb_1d(Toe.n, m, hCe1)
+end
+
+"""
+get_mce_1d(n, cov)
+
+Assembles the minimally circulant embedding structure for the symmetric
+Toeplitz structure of a given stationary covariance cov(Δx). Faster and less
+memory consuming than get_mce_1d(Toe).
+"""
+function get_mce_1d(n::Int, cov)
+
+  dx = 1 / (n - 1)
+  Te1 = Vector{T}(undef, n)
+  for i in 1:n
+    Te1[i] = cov((i - 1) * dx)
+  end
+  Toe = bststb_1d(n, Te1)
+
+
   m = 2 * (Toe.n - 1)
   Ce1 = Vector{T}(undef, m)
   Ce1[1:Toe.n] = Toe.e1
--- MyEmbeddings/MyEmbeddings.jl
+++ MyEmbeddings/MyEmbeddings.jl
@@ -1,11 +1,14 @@
 module MyEmbeddings
 
-export get_cov_toeplitz_1d, get_mce_1d, matvec
+export get_cov_toeplitz_1d, get_mce_1d
+#export get_cov_toeplitz_2d, get_mce_2d
+export matvec#, sample
 
 const T = Float64
 const T32 = Float32
 const T16 = Float16
 
 include("1DEmbeddings.jl")
+#include("2DEmbeddings.jl")
 
 end
